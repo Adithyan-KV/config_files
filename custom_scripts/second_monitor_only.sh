cc6162acf9fee9db60633bfe428b6771378d0099
Adithyan-KV <adithyankv007@gmail.com>
2021-06-25 23:54:34 +0530

    Added basic monitor control script
     -Added the bash script
     -Added relevant readme

---
+++ custom_scripts/second_monitor_only.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+list_available_displays(){
+    #parse xrandr output to get names of all displays
+    xrandr|grep "connected"|awk '{print$1}'
+}
+
+count_displays(){
+    #count the number of display names using \n as seperator
+    list_available_displays|wc|awk '{print$1}'
+}
+
+DISPLAY_COUNT=$(count_displays)
+echo $DISPLAY_COUNT' displays found'
+
+#If there is more than one display available turn off the laptop
+#inbuilt display and set another display as primary
+if [ $DISPLAY_COUNT -ge 2 ]
+then 
+    SECOND_DISPLAY=$(list_available_displays|awk 'FNR == 2 {print}')
+    LAPTOP_DISPLAY=$(list_available_displays|awk 'FNR == 1 {print}')
+    echo 'Setting external display '$SECOND_DISPLAY' as primary...'
+    xrandr --output $SECOND_DISPLAY --primary
+    echo 'turning off laptop display '$LAPTOP_DISPLAY'...'
+    xrandr --output $LAPTOP_DISPLAY --off
+fi
