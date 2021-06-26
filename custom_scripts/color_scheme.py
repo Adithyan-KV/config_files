1e4337e1fe9f6660ea2cdd7fe07132f7d73643b5
Adithyan-KV <adithyankv007@gmail.com>
2021-06-27 05:27:23 +0530

    Printing out the colors to terminal

--- custom_scripts/color_scheme.py
+++ custom_scripts/color_scheme.py
@@ -13,9 +13,22 @@ def main():
     image = pywal.image.get(args.path)
     colors = pywal.colors.get(image)
 
+    # parse the output and print out colors
     for key in colors['colors']:
         hex_code = colors['colors'][key]
-        print(hex_code)
+        print_to_terminal(hex_code)
+
+
+def print_to_terminal(hex_code):
+    reset = '\033[0m'
+    hex_value = hex_code.strip('#')
+    r, g, b = tuple(int(hex_value[i:i+2], 16) for i in (0, 2, 4))
+    escape = get_color_escape(r, g, b, background=False)
+    print(f'{escape}{hex_code}███{reset}')
+
+
+def get_color_escape(r, g, b, background=False):
+    return '\033[{};2;{};{};{}m'.format(48 if background else 38, r, g, b)
 
 
 if __name__ == "__main__":
