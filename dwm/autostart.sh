ce98e1a32f7706fe4c2c153c073cee35cf82f71b
Adithyan-KV <adithyankv007@gmail.com>
2021-08-08 03:57:57 +0530

    Added autostart scripts for dwm

---
+++ dwm/autostart.sh
@@ -0,0 +1,8 @@
+# Remap caps lock key to function as escape
+setxkbmap -option caps:escape
+
+# Set the keyboard repeat rate to be faster
+xset r rate 200 30
+
+# Start the compositor
+picom --experimental-backend&
